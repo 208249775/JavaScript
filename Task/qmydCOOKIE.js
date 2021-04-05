2981055c5ea3189538b9fb55afa1ad73bbd08beb
208249775 <62370276+208249775@users.noreply.github.com>
2021-04-05 18:06:29 +0800

    Create qmydCOOKIE.js

---
+++ Task/qmydCOOKIE.js
@@ -0,0 +1,18 @@
+//独立COOKIE文件     ck在``里面填写，多账号换行
+
+let qmydtokenVal = `eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvcW15ZC55aWNoZW5ndy5jblwvYXBwc1wvbG9naW5cL3dlY2hhdCIsImlhdCI6MTYxNTI4NzQxNSwiZXhwIjoyMDUzNjg4NzQxNSwibmJmIjoxNjE1Mjg3NDE1LCJqdGkiOiJyNHhZcEF5NWI1bFlHaTFwIiwic3ViIjoxMTcxMTksInBydiI6IjQxZGY4ODM0ZjFiOThmNzBlZmE2MGFhZWRlZjQyMzQxMzcwMDY5MGMifQ.T1iQOsaIXsovGALs9yKluN_5iLXW5xFhycpYdjn5Qhg`
+
+
+
+
+
+
+
+
+let qmydcookie = {
+
+    qmydtokenVal: qmydtokenVal,
+
+}
+
+module.exports = qmydcookie
